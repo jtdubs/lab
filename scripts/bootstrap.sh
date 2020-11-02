7a3451dd6e255e7c740b206b0ba6e3b65cd22a45
Justin Dubs <jtdubs@gmail.com>
2020-11-02 13:03:59 -0500

    basic arch build works and boots

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,23 +5,19 @@ set -eu
 echo "Setting clock..."
 timedatectl set-ntp true
 
-
 echo "Partitioning disk..."
 sgdisk --zap-all /dev/sda
 wipefs --all /dev/sda
-
-# EFI boot partition
-sgdisk --new=1:0:+512M --typecode=1:EF00 /dev/sda
+sgdisk --new=1:0:+512M --typecode=1:EF00 --change-name 1:BOOT /dev/sda
 mkfs.fat -F32 /dev/sda1
-
-# Swap
-sgdisk --new=2:0:+2G --typecode=1:8200 /dev/sda
+sgdisk --new=2:0:+2G --typecode=2:8200 --change-name 2:SWAP /dev/sda
 mkswap /dev/sda2
-
-# Root
-sgdisk --new=3:0:0 --typecode=1:8300 /dev/sda
+sgdisk --new=3:0:0 --typecode=3:8300 --change-name 3:ROOT /dev/sda
 mkfs.ext4 -L "Arch Root" /dev/sda3
 
+echo "Creating EFI boot entry..."
+efibootmgr --disk /dev/sda --part 1 --create --label "Arch Linux" --loader /vmlinuz-linux --unicode "root=PARTUUID=$(blkid -s PARTUUID -o value /dev/sda3) rw initrd=\initramfs-linux.img" --verbose
+efibootmgr --bootorder 0004
 
 echo "Mounting filesystems..."
 mount /dev/sda3 /mnt
@@ -29,11 +25,9 @@ mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 swapon /dev/sda2
 
-
 echo "Bootstrapping..."
 pacstrap /mnt base linux linux-firmware man-db man-pages
 genfstab -U /mnt >> /mnt/etc/fstab
-
 
 cat > /mnt/chroot-install.sh <<EOF
 echo "Setting timezone..."
@@ -52,40 +46,42 @@ echo ::1 arch-base >> /etc/hosts
 echo 127.0.1.1 arch-base.localdomain arch-base >> /etc/hosts
 
 echo "Setting root password..."
-usermod -p $(/usr/bin/openssl passwd -crypt 'foobar81') root
+usermod -p $(/usr/bin/openssl passwd -crypt 'toor') root
 
 echo "Configuring network..."
 pacman -S --noconfirm networkmanager dhclient
-systemctl enable networkmanager.service
+systemctl enable NetworkManager.service
 
 echo "Configuring ssh..."
 pacman -S --noconfirm openssh
 systemctl enable sshd.service
 
-echo "Creating packer user..."
-useradd -p $(/usr/bin/openssl passwd -crypt 'packer') -m -U packer
+echo "Creating ansible user..."
+useradd -p $(/usr/bin/openssl passwd -crypt 'ansible') -m -U ansible
 
 echo "Setting up sudo..."
 pacman -S --noconfirm sudo
-cat > /etc/sudoers.d/10_packer <<EOS
+cat > /etc/sudoers.d/10_ansible <<EOS
 Defaults env_keep += "SSH_AUTH_SOCK"
-packer ALL=(ALL) NOPASSWD: ALL
+ansible ALL=(ALL) NOPASSWD: ALL
 EOS
-chmod 0440 /etc/sudoers.d/10_packer
-EOF
+chmod 0440 /etc/sudoers.d/10_ansible
 
+echo "Installing vmware tools..."
+pacman -S --noconfirm linux-headers open-vm-tools nfs-utils
+systemctl enable vmtoolsd.service
+systemctl enable rpcbind.service
+
+echo "Installing updates..."
+pacman -Syu --noconfirm
+EOF
 
 echo "Entering chroot..."
 chmod a+x /mnt/chroot-install.sh
-arch-chroot /mnt /chroot-install.sh
+systemd-nspawn --machine=guest -D /mnt /chroot-install.sh
 echo "Exited chroot..."
-
 
 echo "Cleaning up..."
 rm /mnt/chroot-install.sh
 umount /mnt/boot
 umount /mnt
-
-
-echo "Rebooting..."
-reboot
