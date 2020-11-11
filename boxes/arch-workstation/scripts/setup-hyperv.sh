090e12df9c265ef81ddcd84bb45a10ec1f11d33f
Justin Dubs <jtdubs@gmail.com>
2020-11-11 18:51:43 -0500

    hyper-v works for arch base and workstation

---
+++ boxes/arch-workstation/scripts/setup-hyperv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eu
+
+echo "Installing Hyper-V packages..."
+sudo pacman -S --noconfirm xf86-video-fbdev
---
+++ boxes/arch-workstation/scripts/setup-vmware.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eu
+
+echo "Installing VMWare packages..."
+sudo pacman -S --noconfirm xf86-video-vmware
