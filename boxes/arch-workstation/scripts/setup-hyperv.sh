51fb0384a459f0fde0cf80f565e4a0ab6f734de6
Justin Dubs <jtdubs@gmail.com>
2020-11-11 20:13:53 -0500

    first run at arch-workstation hyper-v enhanced session mode

--- boxes/arch-workstation/scripts/setup-hyperv.sh
+++ boxes/arch-workstation/scripts/setup-hyperv.sh
@@ -4,3 +4,10 @@ set -eu
 
 echo "Installing Hyper-V packages..."
 sudo pacman -S --noconfirm xf86-video-fbdev
+
+echo "Preparing for Enhanced Session Support..."
+yay -S --noconfirm xrdp-git
+git clone https://github.com/Microsoft/linux-vm-tools
+cd linux-vm-tools/arch
+./makepkg.sh
+sudo ./install-config.sh
