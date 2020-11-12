9a2eaac437cb696eedc67f650e7e40cf788445f9
Justin Dubs <jtdubs@gmail.com>
2020-11-12 08:35:02 -0500

    ubuntu-base is good on hyperv

--- boxes/ubuntu-base/scripts/install-hyperv.sh
+++ boxes/ubuntu-base/scripts/install-hyperv.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-pushd /root
-
-echo "Preparing for Enhanced Session Support..."
-git clone https://github.com/Microsoft/linux-vm-tools
-cd linux-vm-tools/ubuntu/18.04
-chmod a+x install.sh
-./install.sh
-
-popd
+# pushd /root
+# 
+# echo "Preparing for Enhanced Session Support..."
+# git clone https://github.com/Microsoft/linux-vm-tools
+# cd linux-vm-tools/ubuntu/18.04
+# chmod a+x install.sh
+# ./install.sh
+# 
+# popd
