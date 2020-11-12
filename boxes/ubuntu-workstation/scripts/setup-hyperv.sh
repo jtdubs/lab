b145a364bafcdc8fde27214e25762a0763093c34
Justin Dubs <jtdubs@gmail.com>
2020-11-12 15:27:21 -0500

    hyper-v work for ubuntu-workstation

--- boxes/ubuntu-workstation/scripts/setup-hyperv.sh
+++ boxes/ubuntu-workstation/scripts/setup-hyperv.sh
@@ -8,3 +8,7 @@ cd linux-vm-tools/ubuntu/18.04
 sed -i 's/^HWE=""/HWE="-hwe-20.04"/' install.sh
 chmod a+x install.sh
 ./install.sh
+
+echo "Fixing xrdp config..."
+sed -i 's/^port=3389/port=vsock:\/\/-1:3389/' /etc/xrdp/xrdp.ini
+sed -i 's/^use_vsock=true/use_vsock=false/' /etc/xrdp/xrdp.ini
