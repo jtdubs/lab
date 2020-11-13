c771384297d16953f470e5baa0a54eea16b8f914
Justin Dubs <jtdubs@gmail.com>
2020-11-12 22:35:45 -0500

    more win10 workstation experimenting

--- boxes/win10-workstation/scripts/Create-User.ps1
+++ boxes/win10-workstation/scripts/Create-User.ps1
@@ -1,5 +1,16 @@
 $ErrorActionPreference="Stop"
 
+# Install First-Logon.ps1 script
+New-Item -Path "C:\ProgramData\Packer" -Type Directory
+Move-Item -Path "C:\Windows\Temp\First-Logon.ps1" -Destination "C:\ProgramData\Packer\First-Logon.ps1"
+
+# Register script in default profile
+reg load HKEY_Users\Temp C:\Users\Default\NTUSER.DAT
+New-ItemProperty -Path "registry::HKU\Temp\Software\Microsoft\Windows\CurrentVersion" -Name "RunOnce" -Value "powershell.exe C:\ProgramData\Packer\First-Logon.ps1" -PropertyType "String"
+[gc]::collect()
+reg unload HKEY_Users\Temp 
+
+# Create user
 New-LocalUser -AccountNeverExpires -FullName "Justin Dubs" -Name jtdubs -Password ([SecureString]::New())
 Add-LocalGroupMember -Group Administrators -Member jtdubs
 
