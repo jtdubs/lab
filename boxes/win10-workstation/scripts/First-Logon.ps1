411211b9eb4db48187edef604b2096475a92a90a
Justin Dubs <jtdubs@gmail.com>
2020-11-13 12:27:09 -0500

    install windows terminal settings...

--- boxes/win10-workstation/scripts/First-Logon.ps1
+++ boxes/win10-workstation/scripts/First-Logon.ps1
@@ -1,8 +1,11 @@
-$ErrorActionPreference="Stop"
+$ErrorActionPreference="Stop" 
 
 # Register packages
 Add-AppxPackage -DisableDevelopmentMode -Register ((Get-AppxPackage -AllUsers "CanonicalGroupLimited.Ubuntu20.04onWindows").InstallLocation + "\AppxManifest.xml")
 Add-AppxPackage -DisableDevelopmentMode -Register ((Get-AppxPackage -AllUsers "Microsoft.WindowsTerminal").InstallLocation + "\AppxManifest.xml")
+
+# Install Windows Terminal settings
+Copy-Item -Path C:\ProgramData\Packer\settings.json -Destination ("$env:LOCALAPPDATA\Packages\" + (Get-AppxPackage "Microsoft.WindowsTerminal").PackageFamilyName + "\LocalState")
 
 # Setup Ubuntu WSL
 ubuntu2004.exe install --root
