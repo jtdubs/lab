df8648018017ba6cd329458e7777219f7971d822
Justin Dubs <jtdubs@gmail.com>
2020-11-13 20:18:48 -0500

    bug fixes

--- boxes/win10-workstation/scripts/Setup-AutoRun.ps1
+++ boxes/win10-workstation/scripts/Setup-AutoRun.ps1
@@ -1,6 +1,5 @@
 $ErrorActionPreference="Stop"
 
-New-Item -Path "registry::HKCU\TSoftware\Microsoft\Windows\CurrentVersion" -Name "RunOnce"
 New-ItemProperty -Path "registry::HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "First Logon" -Value "powershell.exe C:\ProgramData\Vagrant\Run-Once.ps1" -PropertyType "String"
 
 return 0
