ee76cb11008dfda29a3ab26ca47c6f7e3a09ee00
Justin Dubs <jtdubs@gmail.com>
2020-11-13 21:43:22 -0500

    move from wsl2 to wsl

--- boxes/win10-workstation/scripts/Install-Software-1.ps1
+++ boxes/win10-workstation/scripts/Install-Software-1.ps1
@@ -3,6 +3,6 @@ $ErrorActionPreference="Stop"
 choco install --acceptlicense --yes microsoft-windows-terminal
 choco install --acceptlicense --yes powershell-core
 choco install --acceptlicense --yes firefox
-choco install --acceptlicense --yes wsl2
+choco install --acceptlicense --yes wsl
 
 return 0
--- boxes/win10-workstation/scripts/Install-Software-2.ps1
+++ boxes/win10-workstation/scripts/Install-Software-2.ps1
@@ -1,6 +1,5 @@
 $ErrorActionPreference="Stop"
 
-choco install --acceptlicense --yes --force wsl2
 choco install --acceptlicense --yes wsl-ubuntu-2004
 
 return 0
