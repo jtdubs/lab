debd66d49da6c4910e19204352def6570d3bfa97
Justin Dubs <jtdubs@gmail.com>
2020-11-16 07:56:07 -0500

    attempt to turn on RDP and make networks default to private on windows

--- boxes/win10-base/scripts/Enable-WinRM.ps1
+++ boxes/win10-base/scripts/Enable-WinRM.ps1
@@ -1,9 +1,15 @@
 $ErrorActionPreference="Stop"
 
-winrm quickconfig -q
+# Enable WinRM
+winrm quickconfig -q -transport:http -force
 winrm s "winrm/config"              '@{ MaxTimeoutms="600000"      }'
 winrm s "winrm/config/winrs"        '@{ MaxMemoryPerShellMB="1024" }'
 winrm s "winrm/config/service"      '@{ AllowUnencrypted="true"    }'
 winrm s "winrm/config/service/auth" '@{ Basic="true"               }'
+
+# Enable RDP
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+# Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "UserAuthentication" -Value 1
 
 return 0
