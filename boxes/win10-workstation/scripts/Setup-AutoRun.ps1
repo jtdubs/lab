09d0209c2e4ef5638acae1059b4130444437ce75
Justin Dubs <jtdubs@gmail.com>
2020-11-19 13:31:26 -0500

    vagrant ssh, powershell and rdp work out of the box in the ctf env now

--- boxes/win10-workstation/scripts/Setup-AutoRun.ps1
+++ boxes/win10-workstation/scripts/Setup-AutoRun.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference="Stop"
 
+New-Item -Path "registry::HKCU\Software\Microsoft\Windows\CurrentVersion\" -Name "RunOnce" -Force
 New-ItemProperty -Path "registry::HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "First Logon" -Value "powershell.exe C:\ProgramData\Vagrant\Run-Once.ps1" -PropertyType "String"
 
 return 0
