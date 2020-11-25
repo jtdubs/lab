b0c1b7da0b97375dfa747f1a359146340f7ee1f6
Justin Dubs <jtdubs@gmail.com>
2020-11-25 10:11:29 -0500

    ws12core and ws16core images

---
+++ boxes/ws16core-base/scripts/Enable-WinRM.ps1
@@ -0,0 +1,15 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference="Stop"
+
+# Enable WinRM
+winrm quickconfig -q -transport:http -force
+winrm s "winrm/config"              '@{ MaxTimeoutms="600000"      }'
+winrm s "winrm/config/winrs"        '@{ MaxMemoryPerShellMB="1024" }'
+winrm s "winrm/config/service"      '@{ AllowUnencrypted="true"    }'
+winrm s "winrm/config/service/auth" '@{ Basic="true"               }'
+
+# Enable RDP
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+return 0
