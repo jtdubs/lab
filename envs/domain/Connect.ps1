21ad4a655402965d6c56001174ea2f62f2194281
Justin Dubs <jtdubs@gmail.com>
2020-12-04 19:49:45 -0500

    Make connect script handle RDP and SSH

--- envs/domain/Connect.ps1
+++ envs/domain/Connect.ps1
@@ -6,20 +6,24 @@ param (
 Set-StrictMode -Version Latest
 $ErrorActionPreference="Stop"
 
-
 $user = "LAB\Administrator"
 $pass = ConvertTo-SecureString -String "sup3rs3cr3t!" -AsPlainText -Force
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $pass
 
-$port = (
-    vagrant port --machine-readable $Server
-  | ConvertFrom-Csv
-  | ? {
-       $_.metadata -eq "forwarded_port" -and $_.provider -eq 5985
-    }
-).virtualbox
+$ports = vagrant port --machine-readable $Server `
+  | ConvertFrom-Csv `
+  | Where-Object -Property "metadata" -EQ -Value "forwarded_port"
 
-Enter-PSSession `
-    -Credential $cred `
-    -ComputerName localhost `
-    -Port $port
+$rdpPort = $ports | Where-Object -Property "provider" -EQ -Value 5985
+$sshPort = $ports | Where-Object -Property "provider" -EQ -Value 22
+
+if ($rdpPort) {
+    Write-Host "Connecting via PS Remoting..."
+    Enter-PSSession `
+        -Credential $cred `
+        -ComputerName localhost `
+        -Port $rdpPort.virtualbox
+} elseif ($sshPort) {
+    Write-Host "Connecting via ssh..."
+    ssh -i ../../vagrant.id_rsa -o "StrictHostKeyChecking=no" vagrant@localhost -p $sshPort.virtualbox
+}
