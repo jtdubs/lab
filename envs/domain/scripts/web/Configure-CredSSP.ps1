541a9bb77a9d0f50157b22c8f374f827f32742be
Justin Dubs <jtdubs@gmail.com>
2020-12-07 18:33:44 -0500

    factor out common scripts

--- envs/domain/scripts/web/Configure-CredSSP.ps1
+++
@@ -1,6 +0,0 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
-
-Write-Host "Enabling CredSSP delegation within domain..."
-Enable-WSManCredSSP -Force -Role Client -DelegateComputer "*.lab.dubs.zone"
-Enable-WSManCredSSP -Force -Role Server
---
+++ envs/domain/scripts/windows/Summary.ps1
@@ -0,0 +1,33 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+Write-Host "Computer Info:"
+Get-ComputerInfo `
+    | Select-Object -Property `
+        CsName, CsDomain, CsDomainRole, CsPCSystemType,
+        OsName, OsVersion,
+        @{ L="TotalMemory"; E={ "{0:N2}MB" -f ($_.OsTotalVisibleMemorySize / 1024) } },
+        @{ L="FreeMemory"; E={ "{0:N2}MB" -f ($_.OsFreePhysicalMemory / 1024) } } `
+    | Out-Host
+
+Write-Host "Disk Info:"
+Get-WmiObject -Class Win32_LogicalDisk `
+    | Where-Object -Property DriveType -EQ -Value 3 `
+    | Select-Object -Property DeviceID, VolumeName,
+        @{ L="Available"; E={ "{0:N2}GB" -f ($_.FreeSpace / 1GB) } },
+        @{ L="Capacity"; E={ "{0:N2}GB" -f ($_.Size/ 1GB) } } `
+    | Format-Table -AutoSize `
+    | Out-Host
+
+Write-Host "Domain Info:"
+Get-ADDomain `
+    | Select-Object -Property Forest,DomainMode,DNSRoot,NetBIOSName,PDCEmulator,InfrastructureMaster `
+    | Out-Host
+
+Write-Host "Local Users:"
+Get-LocalUser `
+    | Out-Host
+
+Write-Host "Local Administrators:"
+Get-LocalGroupMember Administrators `
+    | Out-Host
